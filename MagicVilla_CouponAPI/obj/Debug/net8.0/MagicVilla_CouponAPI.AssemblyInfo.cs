b54d8b897578753eda52d739daa75959df7df64f
RubenzOfficenet <rgarrido@officenet.com.mx>
2026-05-26 21:34:21 -0600

    se comunica con sql server del container

--- MagicVilla_CouponAPI/obj/Debug/net8.0/MagicVilla_CouponAPI.AssemblyInfo.cs
+++ MagicVilla_CouponAPI/obj/Debug/net8.0/MagicVilla_CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a931805500820d7149a43776d40d48aa759e067")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
